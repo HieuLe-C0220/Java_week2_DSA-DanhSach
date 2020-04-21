ce0de08340d857d7e68226133f38cd33923a2d8c
Hieu.Le <letrunghieu2602@gmail.com>
2020-04-22 00:13:01 +0700

    done

---
+++ LinkedList/src/TestMyLinkedList.java
@@ -0,0 +1,13 @@
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10); //Tạo 1 linked list có 1 phần tử 10 đầu tiên
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+
+        ll.add(4,9);
+        ll.add(4,9);
+        ll.printList();
+    }
+}
